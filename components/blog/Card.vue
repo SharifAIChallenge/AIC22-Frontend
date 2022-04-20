c4d865e5ad5a433e3f9fe0108bbdf65f80cdaf3a
Meraj Sheikhha <meraj.sheikhhaa@gmail.com>
2022-04-21 01:12:53 +0430

    add blog section

---
+++ components/blog/Card.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-card
+      class="blog-card py-2"
+      elevation="0"
+  >
+    <v-card-title
+        class="py-0 my-2"
+    >
+      {{ title }}
+    </v-card-title>
+    <v-card-text
+        class="pt-8"
+    >
+      {{ description }}
+    </v-card-text>
+    <v-flex class="d-flex px-4 align-center justify-space-between">
+      <v-card-text class="caption ma-0 pa-0">
+        {{ date }}
+      </v-card-text>
+      <v-btn
+          class="read-more"
+          :to="href"
+          plain
+          text
+      >
+        مطالعه
+        <v-icon>
+          mdi-chevron-left
+        </v-icon>
+      </v-btn>
+    </v-flex>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    description: String,
+    href: String,
+    date: String,
+  },
+  name: "Card"
+}
+</script>
+
+<style scoped lang="scss">
+.blog-card {
+  background-color: #13202e;
+
+  .v-card__title {
+    border-right: 4px solid #0071e3;
+  }
+  .v-card__text{
+    color: rgba(255, 255, 255, 0.7);
+  }
+  .read-more{
+    border-bottom: 1px solid #182636;
+  }
+}
+
+</style>
---
+++ components/landing/Blog.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <h3 class="text-h2 text-center">
+      آخرین اخبار
+    </h3>
+    <v-container class="mt-16">
+      <box>
+        <div class="pa-14">
+          <v-row>
+            <v-col class="col-12 col-md-4">
+              <Card
+                  title="افتتاحیه"
+                  description="یھ سری اخبار مروط بھ رویداد یھ سری اخبار مروط بھ رویداد یھ سری اخبار مروط ب"
+                  date="فروردین"
+                  href="https://google.com"
+              />
+            </v-col>
+            <v-col class="col-12 col-md-4">
+              <Card
+                  title="افتتاحیه"
+                  description="یھ سری اخبار مروط بھ رویداد یھ سری اخبار مروط بھ رویداد یھ سری اخبار مروط ب"
+                  date="فروردین"
+                  href="https://google.com"
+              />
+            </v-col>
+            <v-col class="col-12 col-md-4">
+              <Card
+                  title="افتتاحیه"
+                  description="یھ سری اخبار مروط بھ رویداد یھ سری اخبار مروط بھ رویداد یھ سری اخبار مروط ب"
+                  date="فروردین"
+                  href="https://google.com"
+              />
+            </v-col>
+          </v-row>
+          <div class="text-center mt-10">
+            <v-btn
+                color="primary"
+                class="px-8"
+                rounded
+            >
+              بیشتر...
+            </v-btn>
+          </div>
+
+        </div>
+      </box>
+    </v-container>
+  </div>
+</template>
+
+<script>
+import Box from "~/components/utilities/Box";
+import Card from "~/components/blog/Card";
+
+export default {
+  name: "Blog",
+  components: {Card, Box}
+}
+</script>
+
+<style scoped>
+
+</style>
