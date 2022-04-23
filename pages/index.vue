ebd2b69ab9cb44f195e00905b249ee8d132686cb
Meraj Sheikhha <meraj.sheikhhaa@gmail.com>
2022-04-23 23:13:26 +0430

    update about section

--- pages/index.vue
+++ pages/index.vue
@@ -12,6 +12,7 @@
     <!--      :isIntersecTrophy="isIntersec.Trophy"-->
     <!--    />-->
     <About :onIntersecAbout="onIntersecAbout"/>
+    <Statics/>
     <Video :onIntersecVideo="onIntersecVideo"/>
     <Statistics/>
     <TimeLine :onIntersecTimeline="onIntersecTimeline"/>
@@ -34,13 +35,14 @@ import Statistics from '~/components/landing/old/Statistics';
 import TrophyDesktop from '~/components/landing/old/TrophyDesktop';
 import TrophyMobile from '~/components/landing/old/TrophyMobile';
 import Social from '~/components/landing/old/Social.vue';
-import About from '~/components/landing/old/About.vue';
+import About from '~/components/landing/About.vue';
 import Sponsor from '~/components/landing/old/Sponsor.vue';
 import HeadTop from '~/components/landing/HeadTop.vue';
 import CallToAction from '~/components/CallToAction.vue';
 import Staff from '~/components/landing/old/staff/Staff';
 import EmaliCallToAction from '~/components/EmailCallToAction.vue';
 import Blog from "~/components/landing/Blog";
+import Statics from "~/components/landing/Statics";
 
 export default {
   components: {
@@ -59,6 +61,7 @@ export default {
     Staff,
     CallToAction,
     EmaliCallToAction,
+    Statics
   },
   layout: 'landing',
   EmaliCallToActionh: false,
