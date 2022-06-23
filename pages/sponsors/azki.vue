4a81fc024af725e88577acc5372d5faa99e912db
Amirzgh1 <Amirgh5665@gmail.com>
2022-06-23 20:38:02 +0430

    fix some problems in sponsor

--- pages/sponsors/azki.vue
+++ pages/sponsors/azki.vue
@@ -1,76 +1,16 @@
 <template>
 <div>
   <Sponsor class="mt-15"></Sponsor>
-    
-  <div class="mb-8">
-    <TitleContainer title="فرصت های شغلی"></TitleContainer>
-
-
-    <v-container>
-      <v-row>
-        <v-col cols="12" md="6" v-for="post in posts" :key="post.link">
-          <open-position-card :title="post.title" :link="post.link" />
-        </v-col>
-      </v-row>
-    </v-container>
-  </div>
+  <AzkiOpenPositions/>
 </div>
 </template>
 
 <script>
-import TitleContainer from "~/components/TitleContainer";
-import OpenPositionCard from "~/components/utilities/OpenPositionCard";
-import Sponsor from "~/components/landing/Sponsor.vue";
+import AzkiOpenPositions from '~/components/sponsors/azkiOpenPositions.vue';
 
 export default {
-  name: "azki",
-  components: { OpenPositionCard, TitleContainer, Sponsor },
-  data(){
-    return{
-      posts:[
-        {
-          title: "Data Engineer",
-          link: "https://jobs.azki.com/jobs/52d067"
-        },
-          {
-          title: "Senior QA Software Testing Specialist",
-          link: "https://jobs.azki.com/jobs/251c3c"
-        },
-          {
-          title: "Senior UI/UX Designer",
-          link: "https://jobs.azki.com/jobs/e44c08"
-        },
-          {
-          title: "QA Team Lead",
-          link: "https://jobs.azki.com/jobs/008cf5"
-        },
-          {
-          title: "UX Researcher",
-          link: "https://jobs.azki.com/jobs/e84443"
-        },
-          {
-          title: "UX Writer",
-          link: "https://jobs.azki.com/jobs/a379ee"
-        },
-          {
-          title: "Senior Data Analyst",
-          link: "https://jobs.azki.com/jobs/5d450c"
-        },
-        {
-          title: "Product Manager",
-          link: "https://jobs.azki.com/jobs/5cea4f"
-        },
-        {
-          title: "Senior Java Developer",
-          link: "https://jobs.azki.com/jobs/7ac3ca"
-        },
-        {
-          title: "Senior PHP Developer",
-          link: "https://jobs.azki.com/jobs/7ade90"
-        },
-      ]
-    }
-  }
+    name: "azki",
+    components: { AzkiOpenPositions }
 }
 </script>
 
