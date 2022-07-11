8fc9aaa5c74e31f5fe7b964a491e21cdb36610ad
Soroush Sherafat <sorousherafat@gmail.com>
2022-07-11 15:21:16 +0430

    FEAT(MyTeam): added MyTeam.vue. lacks some functionalities.

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ import fa from 'vuetify/es5/locale/fa';
 require('dotenv').config();
 
 
-// const [MODE, a] = process.env.TOF_MODE;
+// const [MODE, a] = process..env.TOF_MODE;
 const MODE = process.env.MODE;
 
 console.log(MODE);
@@ -157,7 +157,7 @@ export default {
         // baseURL: MODE == 's' ? 'https://stg.aichallenge.ir/api/' : 'https://aichallenge.ir/api/',
         // browserBaseURL: MODE == 's' ? 'https://stg.aichallenge.ir/api/' : 'https://aichallenge.ir/api/',
         // browserBaseURL: 'https://aichallenge.sharif.edu/api',
-        // baseURL: process.env.baseURL,
+        // baseURL: process..env.baseURL,
         // baseURL: 'https://aichallenge.sharif.edu/api',
         // baseURL: 'http://172.17.0.1:8000/api'
 
