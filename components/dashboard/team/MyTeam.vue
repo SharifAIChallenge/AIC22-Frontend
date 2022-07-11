a67412097ec3af674500c8ad270bb53b6969512c
Soroush Sherafat <sorousherafat@gmail.com>
2022-07-11 20:29:53 +0430

    FEAT(MyTeam): edit team.

--- components/dashboard/team/MyTeam.vue
+++ components/dashboard/team/MyTeam.vue
@@ -7,14 +7,18 @@
             <div class="team-card__picture">
               <img
                 v-if="this.team.image"
-                :src="imageDisplay ? imageDisplay : this.team.image"
+                :src="this.form.imageUrl ? this.form.imageUrl : this.team.image_url"
                 class="rounded-circle"
                 alt="team profile image"
+                width="150"
+                height="150"
               />
               <img v-else class="rounded-circle" src="~/assets/images/avatar-sample.svg" alt="default team profile image" />
             </div>
             <div v-if="!isEditing" class="team-card__edit">
-              <v-icon right class="team-card__edit__icon" color="rgba(255, 255, 255, 0.12)" @click="toggleIsEditing">mdi-pencil</v-icon>
+              <v-icon right class="team-card__edit__icon" color="rgba(255, 255, 255, 0.12)" @click="toggleIsEditing">
+                mdi-pencil
+              </v-icon>
             </div>
           </div>
           <div v-if="!isEditing" class="team-card__content">
@@ -30,19 +34,19 @@
                     class="team-card__member__name inherit-bg-color"
                     v-text="member.profile.firstname_fa + ' ' + member.profile.lastname_fa"
                   />
-                  <v-list-item-icon class="team-card__member__icon inherit-bg-color">
-                    <v-icon color="rgba(255, 255, 255, 0.12)">mdi-account-box</v-icon>
-                  </v-list-item-icon>
+<!--                  <v-list-item-icon class="team-card__member__icon inherit-bg-color">-->
+<!--                    <v-icon color="rgba(255, 255, 255, 0.12)">mdi-account-box</v-icon>-->
+<!--                  </v-list-item-icon>-->
                 </div>
               </v-list-item>
-              <v-list-item class="team-card__add-member" v-if="this.team.members.length < 3" dense>
-                <div class="team-card__add-member__content inherit-bg-color">
-                  <v-list-item-title class="team-card__add-member__name inherit-bg-color">افزودن عضو جدید</v-list-item-title>
-                  <v-list-item-icon class="team-card__add-member__icon inherit-bg-color">
-                    <v-icon color="#20c9b2">mdi-plus</v-icon>
-                  </v-list-item-icon>
-                </div>
-              </v-list-item>
+<!--              <v-list-item class="team-card__add-member" v-if="this.team.members.length < 3" dense>-->
+<!--                <div class="team-card__add-member__content inherit-bg-color">-->
+<!--                  <v-list-item-title class="team-card__add-member__name inherit-bg-color">افزودن عضو جدید</v-list-item-title>-->
+<!--                  <v-list-item-icon class="team-card__add-member__icon inherit-bg-color">-->
+<!--                    <v-icon color="#20c9b2">mdi-plus</v-icon>-->
+<!--                  </v-list-item-icon>-->
+<!--                </div>-->
+<!--              </v-list-item>-->
             </v-list>
             <div class="team-card__leave mt-6">
               <v-btn block color="secondary" class="my-4" @click="leaveTeam" rounded>ترک تیم</v-btn>
@@ -55,14 +59,23 @@
                 class="team-edit__profile__input mt-4"
                 accept="image/png, image/jpeg, image/bmp"
                 placeholder="تصویر جدید"
-                prepend-icon="mdi-camera"
+                prepend-icon="mdi-image-plus"
                 label="افزودن تصویر جدید"
                 v-on:change="onPictureChange"
                 hide-input
               ></v-file-input>
             </div>
-            <v-form ref="editTeamName" onSubmit="return false;" @submit="updateTeamName">
-              <v-text-field v-model="form.name" label="نام جدید تیم" required :rules="requiredRules" class="team-edit__name__field my-5" outlined rounded dense />
+            <v-form ref="editTeamName" v-model="valid" onSubmit="return false;" @submit="updateTeamName">
+              <v-text-field
+                v-model="form.name"
+                label="نام جدید تیم"
+                required
+                :rules="requiredRules"
+                class="team-edit__name__field my-5"
+                outlined
+                rounded
+                dense
+              />
               <div>
                 <v-btn block color="primary" type="submit" :disabled="!valid" rounded>
                   ویرایش نام تیم
@@ -89,14 +102,13 @@ export default {
   props: { toggleHaveTeam: Function },
   async fetch() {
     this.team = await this.$axios.$get('team');
-    this.form = this.team;
-    this.form ={ name : this.form.name, image: this.form.image_url}
   },
   data() {
     return {
       form: {
         name: '',
-        image: '',
+        imageUrl: '',
+        image: null,
       },
       team: {
         members: [],
@@ -109,37 +121,65 @@ export default {
     };
   },
   methods: {
-    onPictureChange(e) {
+    onPictureChange(image) {
       // TODO implement uploading the new image
-      this.imageDisplay = URL.createObjectURL(e);
-      this.team.image = e;
+      this.form.imageUrl = URL.createObjectURL(image);
+      this.form.image = image;
+
+      const formData = new FormData();
+      formData.append('name', this.team.name);
+      formData.append('image', this.form.image);
+
+      this.loading = true;
+
+      this.$axios
+        .$put('team/', formData, { headers: { 'content-type': 'multipart/form-data' } })
+        .then(response => {
+          this.loading = false;
+          this.team = response;
+          this.$toast.success('عکس جدید ذخیره شد!');
+          this.toggleIsEditing();
+        })
+        .catch(error => {
+          this.loading = false;
+          this.form.image = null;
+          this.form.imageUrl = '';
+          this.$toast.error('تغییر عکس با مشکل روبه‌رو شد!');
+          this.toggleIsEditing();
+        });
     },
     leaveTeam() {
       this.loading = true;
-      this.$axios.$delete('team').then(res => {
-        this.loading = false;
-        if (res.status_code === 200 || res.status_code === 204) {
+      this.$axios
+        .$delete('team')
+        .then(res => {
+          this.loading = false;
           this.$toast.success('شما با موفقیت از تیم خارج شدید!');
           this.toggleHaveTeam();
-        } else this.$toast.error('خروج با مشکل مواجه شد!');
-      });
+        })
+        .catch(error => {
+          this.$toast.error('خروج با مشکل مواجه شد!');
+        });
     },
     updateTeamName() {
       this.loading = true;
       this.$axios
-        .$put('team', {
-          name: this.team.name,
+        .$put('team/', {
+          name: this.form.name,
         })
         .then(response => {
           this.loading = false;
-          if (response.status_code === 200) {
+          if (response.name === this.form.name) {
             this.team.name = this.form.name;
             this.form.name = '';
             this.$toast.success('نام تیم با موفقیت تغییر کرد!');
           } else this.$toast.error('تغییر نام با مشکل روبه‌رو شد!');
+          this.toggleIsEditing();
         })
         .catch(error => {
-          console.log(JSON.stringify(error));
+          this.loading = false;
+          this.$toast.error('تغییر نام با مشکل روبه‌رو شد!');
+          this.toggleIsEditing();
         });
     },
     resetTeamNameForm() {
@@ -182,8 +222,8 @@ export default {
   &__picture {
     transform: translateY(-60px);
     object-fit: cover;
-    width: 150px;
-    height: 150px;
+    width: 150px !important;
+    height: 150px !important;
   }
 
   &__edit {
@@ -258,9 +298,7 @@ export default {
 }
 
 .team-edit {
-
   &__name {
-
     &__field {
       height: 36px !important;
     }
