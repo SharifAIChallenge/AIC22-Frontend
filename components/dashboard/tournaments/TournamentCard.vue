b22472c11f4ffe785b54c59f0e7143dbcac14143
Meraj Sheikhha <meraj.sheikhhaa@gmail.com>
2022-08-04 21:47:26 +0430

    update

--- components/dashboard/tournaments/TournamentCard.vue
+++ components/dashboard/tournaments/TournamentCard.vue
@@ -1,54 +1,37 @@
 <template>
-  <div :class="['tournamentCard active d-flex flex-column pa-4']">
-    <h3 class="mb-4">{{ tournament.name }}</h3>
-    <div class="mb-4">
-      <div class="d-flex mb-2">
-        <v-icon class="ml-2">mdi-calendar-clock</v-icon>
-        <span class="ml-2">شروع:</span>
-        <date-time-formatter :date="tournament.start_time" />
-      </div>
-      <!-- <div class="d-flex mb-2">
-        <span class="ml-2">پایان:</span>
-        <date-time-formatter :date="tournament.end_time" />
-      </div> -->
+  <v-card :class="`invites ${status === 'pending' ? '' : 'disable'} rounded px-10 pt-10`">
+    <h5 class="text-h5 text-center">
+      {{ name }}
+    </h5>
+
+    <div class="px-3 acinvites d-flex justify-center" v-if="status === 'pending'">
+      <v-btn class="ml-1" rounded color="primary">تایید</v-btn>
     </div>
-    <v-btn class="py-5 card-btn" :to="`/dashboard/scoreboard?id=${tournament.id}`">
-      مشاهده جدول امتیازات
-    </v-btn>
-    <v-btn class="py-5 mt-3 card-btn" :to="`/dashboard/games?id=${tournament.id}`">
-      مشاهده بازی‌ها
-    </v-btn>
-  </div>
+    <v-row v-else>
+      <v-btn class="col-6" rounded color="secondary" disabled>
+        <span v-if="status === 'accepted'">تایید شده</span>
+        <span v-else>رد شده</span>
+      </v-btn>
+    </v-row>
+  </v-card>
 </template>
 
 <script>
 import DateTimeFormatter from '../../DateTimeFormatter';
 
 export default {
-  components: { DateTimeFormatter },
-  props: {
-    tournament: Object,
-  },
+  components: {DateTimeFormatter},
+  props: ['id', 'name', 'start_time', 'status'],
 };
 </script>
 <style lang="scss">
-.tournament {
-  .tournamentCard {
-    width: 100%;
-    background-color: #141432;
-    a {
-      background-color: #1a1940 !important;
-    }
-  }
-  .active {
-    background-color: #1a1940;
-  }
-  .card-btn {
-    border: 2px solid white;
-    &:hover {
-      // color: var(--v-primary-base);
-      // border: 2px solid var(--v-primary-base);
-    }
-  }
+.invites {
+  background-color: #182636 !important;
 }
+
+.invites.disable {
+  background-color: unset !important;
+  border: 2px dashed #868686 !important;
+}
+
 </style>
--- pages/dashboard/tournaments.vue
+++ pages/dashboard/tournaments.vue
@@ -1,60 +1,77 @@
 <template>
   <div class="tournaments">
-    <SectionHeader title="تورنومنت ها" icon="mdi-tournament" />
+    <v-container class="pa-0 d-flex align-center justify-space-between">
+
+      <p class="headline py-5 ma-0 mr-2">
+        تورنومنت ها
+      </p>
+    </v-container>
+    <v-divider/>
+
     <div class="px-6 px-md-12">
-      <TournamentHeader :header="header"></TournamentHeader>
-      <v-card color="basil">
-        <!-- <v-card-title class="text-center justify-center py-6">
-          <h1 class="font-weight-bold display-3 basil--text">
-            BASiL
-          </h1>
-        </v-card-title> -->
+      <div class="gradient">
+        asd
+      </div>
+      <div>
+        <v-row>
+          <v-col cols="6" md="3" v-for="(list, index) in tournaments" :key="index">
+            <TournamentCard name="test" status="pending" id="1"/>
 
-        <v-tabs v-model="tabs" color="primary" grow>
-          <v-tab>
-            دوستانه
-          </v-tab>
-          <v-tab>
-            سیدبندی
-          </v-tab>
-        </v-tabs>
+          </v-col>
+        </v-row>
+      </div>
+      <!--      <v-card color="basil">-->
+      <!--        &lt;!&ndash; <v-card-title class="text-center justify-center py-6">-->
+      <!--          <h1 class="font-weight-bold display-3 basil&#45;&#45;text">-->
+      <!--            BASiL-->
+      <!--          </h1>-->
+      <!--        </v-card-title> &ndash;&gt;-->
 
-        <v-tabs-items v-model="tabs">
-          <v-tab-item>
-            <v-card color="basil" flat>
-              <v-card-text>
-                <v-row>
-                  <v-col cols="12" sm="6" md="4" xl="3" class="tournament my-4" v-for="tournament in tournaments" :key="tournament.id">
-                    <div>
-                      <TournamentCard :tournament="tournament"></TournamentCard>
-                    </div>
-                  </v-col>
-                </v-row>
-              </v-card-text>
-            </v-card>
-          </v-tab-item>
-          <v-tab-item>
-            <v-card color="basil" flat>
-              <v-card-text>
-                <v-row v-if="finalTournaments.length">
-                  <v-col cols="12" sm="6" md="4" xl="3" class="tournament my-4" v-for="tournament in finalTournaments" :key="tournament.id">
-                    <div>
-                      <TournamentCard :tournament="tournament"></TournamentCard>
-                    </div>
-                  </v-col>
-                </v-row>
-                <div v-else>
-                  <div class="d-flex justify-center py-15">
-                    <h3>
-                      اوپس! تورنمنتی یافت نشد
-                    </h3>
-                  </div>
-                </div>
-              </v-card-text>
-            </v-card>
-          </v-tab-item>
-        </v-tabs-items>
-      </v-card>
+      <!--        <v-tabs v-model="tabs" color="primary" grow>-->
+      <!--          <v-tab>-->
+      <!--            دوستانه-->
+      <!--          </v-tab>-->
+      <!--          <v-tab>-->
+      <!--            سیدبندی-->
+      <!--          </v-tab>-->
+      <!--        </v-tabs>-->
+
+      <!--        <v-tabs-items v-model="tabs">-->
+      <!--          <v-tab-item>-->
+      <!--            <v-card color="basil" flat>-->
+      <!--              <v-card-text>-->
+      <!--                <v-row>-->
+      <!--                  <v-col cols="12" sm="6" md="4" xl="3" class="tournament my-4" v-for="tournament in tournaments" :key="tournament.id">-->
+      <!--                    <div>-->
+      <!--                      <TournamentCard :tournament="tournament"></TournamentCard>-->
+      <!--                    </div>-->
+      <!--                  </v-col>-->
+      <!--                </v-row>-->
+      <!--              </v-card-text>-->
+      <!--            </v-card>-->
+      <!--          </v-tab-item>-->
+      <!--          <v-tab-item>-->
+      <!--            <v-card color="basil" flat>-->
+      <!--              <v-card-text>-->
+      <!--                <v-row v-if="finalTournaments.length">-->
+      <!--                  <v-col cols="12" sm="6" md="4" xl="3" class="tournament my-4" v-for="tournament in finalTournaments" :key="tournament.id">-->
+      <!--                    <div>-->
+      <!--                      <TournamentCard :tournament="tournament"></TournamentCard>-->
+      <!--                    </div>-->
+      <!--                  </v-col>-->
+      <!--                </v-row>-->
+      <!--                <div v-else>-->
+      <!--                  <div class="d-flex justify-center py-15">-->
+      <!--                    <h3>-->
+      <!--                      اوپس! تورنمنتی یافت نشد-->
+      <!--                    </h3>-->
+      <!--                  </div>-->
+      <!--                </div>-->
+      <!--              </v-card-text>-->
+      <!--            </v-card>-->
+      <!--          </v-tab-item>-->
+      <!--        </v-tabs-items>-->
+      <!--      </v-card>-->
     </div>
   </div>
 </template>
@@ -66,20 +83,19 @@ import TournamentHeader from '~/components/dashboard/tournaments/TournamentHeade
 import TournamentCard from '~/components/dashboard/tournaments/TournamentCard';
 
 export default {
-  components: { SectionHeader, SectionContainer, TournamentHeader, TournamentCard },
+  components: {SectionHeader, SectionContainer, TournamentHeader, TournamentCard},
   layout: 'dashboard',
   transition: 'fade-transition',
   async asyncData({ $axios }) {
     let res = await $axios.get('challenge/tournament');
-    let data = res.data.data;
-    // console.log(data);
+    let data = res.data;
     let tournaments = data.filter(t => t.type !== 'final');
     let finalTournaments = data.filter(t => t.type === 'final');
 
-    res = await $axios.get('challenge/tournament/next');
-    let header = res.data.data;
+    // res = await $axios.get('challenge/tournament/next');
+    // let header = res.data.data;
     // console.log(res.data.data);
-    return { tournaments, header, finalTournaments };
+    return { tournaments, finalTournaments };
   },
   data() {
     return {
@@ -92,20 +108,27 @@ export default {
 };
 </script>
 <style lang="scss">
-@import '~/assets/mixins.scss';
-.tournaments {
-  .v-tab {
-    color: white !important;
-    &.v-tab--active {
-      color: var(--v-primary-base) !important;
-    }
-  }
+.gradient {
+  position: relative;
+}
 
-  .tournament {
-    width: 32%;
-    @include v-not-sm {
-      width: 100%;
-    }
-  }
+.gradient::before {
+  position: absolute;
+  content: "";
+
+  z-index: -1;
+  height: 104%;
+  width: 104%;
+  border-radius: 50%;
+  //position: absolute;
+  margin: auto;
+  -webkit-animation: logo 5s linear infinite;
+  -moz-animation: logo 5s linear infinite;
+  animation: logo 5s linear infinite;
+  //transform: scale(0.85);
+  filter: blur(20px);
+  background-image: linear-gradient(
+          0, #FC147F, #480AC2, #E08250, #3AE4F6);
+  opacity: 1;
 }
 </style>
